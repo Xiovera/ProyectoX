0acc6d876efbd7cfc9a91a93b6429a818bdf7881
Rocio Yovera <rocioyovera@ gmail.com>
2017-01-26 16:04:54 -0500

    js 40%

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 function Chat()
 {
+	this.nombre ='';
 	this.people = [];
 	this.messages = [];
 	this.chatAvatar = '';
@@ -12,7 +13,7 @@ function Message(_message,_sender);
 	this.received = false;
 }
 
-function Persona (_name,_avatar);
+function Person(_name,_avatar);
 {
 	this.name = _name;
 	this.avatar = _avatar;
@@ -24,5 +25,36 @@ function Whatsapp()
 	this.selectedChat = null;
 	this.searchChat = function(_keyword){};
 	this.getChatFromId = function(_chatId){};
-	this.
+	this.drawChatList = function(_htmlTarget){
+		var ulChatList = document.getElementById('chat-list');
+
+		for (var i in this.chats){
+			console.log(this.chats[i].messages);
+			var htmlChatList = '<li><div class="avatar">'+
+								'<img src="'+this.chats[i].chatAvatar + '"alt=""class="wh-44">'+
+								'<h4 class="w-contact-name">' + this.chats[i].nombre + '</h4>' +
+								'<p class="w-last-message">' + this.chats [i].messages[this.chats[i].messages.length-1].message + '<'	}
+	}
+}	
+
+function cambiaHeader()
+{
+	imgContacto.src =this.childNotes[1].src;
+	nombreContacto.textContent = this.childNotes[3].textContent;
 }
+
+var search = document.getElementById("search"),
+	contacto = document.getElementByTagName("h4"),
+	forEach = Array.prototype.forEach;
+
+search.addEventListener("keyup",function(e){
+	var choice=this.value;
+
+	forEach.call(contacto,function(f){
+		if(f.innerHTML.toLowerCase().search(choice.toLowerCase()==-1)
+			f.parentNode.parentNode.style.display="none";
+		else
+			f.parentNode.parentNode.style.display="block"})
+	}
+})
+
