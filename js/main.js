6b0e283c12a027abf451cbf8c5f266334a86506f
Rocio Yovera <rocioyovera@gmail.com>
2017-01-30 06:35:47 -0500

    change js

--- js/main.js
+++ js/main.js
@@ -1,60 +1,52 @@
-function Chat()
-{
-	this.nombre ='';
-	this.people = [];
-	this.messages = [];
-	this.chatAvatar = '';
-}
+window.addEventListener("load", cargaPag);
 
-function Message(_message,_sender);
-{
-	this.message = _message;
-	this.sender = _sender;
-	this.received = false;
-}
+var mensaje = document.getElementById("mensajes");
+var contenedor = document.getElementById("conversacion");
+var ultMensaje = document.getElementById("mensaje");
+var ultHora = document.getElementById("hora");
+var chat = document.getElementsByClassName("avatar");
+var imgContacto = document.getElementById("imgContacto");
+var nombreContacto = document.getElementById("nombreContacto");
 
-function Person(_name,_avatar);
-{
-	this.name = _name;
-	this.avatar = _avatar;
-}
-
-function Whatsapp()
-{
-	this.chats = [];
-	this.selectedChat = null;
-	this.searchChat = function(_keyword){};
-	this.getChatFromId = function(_chatId){};
-	this.drawChatList = function(_htmlTarget){
-		var ulChatList = document.getElementById('chat-list');
-
-		for (var i in this.chats){
-			console.log(this.chats[i].messages);
-			var htmlChatList = '<li><div class="avatar">'+
-								'<img src="'+this.chats[i].chatAvatar + '"alt=""class="wh-44">'+
-								'<h4 class="w-contact-name">' + this.chats[i].nombre + '</h4>' +
-								'<p class="w-last-message">' + this.chats [i].messages[this.chats[i].messages.length-1].message + '<'	}
+function cargaPag(){
+	mensaje.addEventListener("keyup", subirMensaje);
+	for (var i = 1; i <= chat.length-2; i++) {
+		chat[i].addEventListener("click", cambiaHeader);
 	}
-}	
-
-function cambiaHeader()
-{
-	imgContacto.src =this.childNotes[1].src;
-	nombreContacto.textContent = this.childNotes[3].textContent;
 }
-
-var search = document.getElementById("search"),
-	contacto = document.getElementByTagName("h4"),
-	forEach = Array.prototype.forEach;
-
-search.addEventListener("keyup",function(e){
-	var choice=this.value;
-
-	forEach.call(contacto,function(f){
-		if(f.innerHTML.toLowerCase().search(choice.toLowerCase()==-1)
-			f.parentNode.parentNode.style.display="none";
-		else
-			f.parentNode.parentNode.style.display="block"})
+function subirMensaje(e){
+	var tecla = e.keyCode;
+	if(tecla == 13 && mensaje.value.trim().length > 0){
+		apareceMensaje();
+		mensaje.value = "";
 	}
-})
+}
+function apareceMensaje(){
+	var envoltura = document.createElement("div");
+	var cajaTexto = document.createElement("div");
+	var	texto = document.createElement("p");
+	var contenHora = document.createElement("div");
+
+	envoltura.classList.add("w-message", "w-message-out");
+	cajaTexto.classList.add("w-message-text");
+	contenHora.classList.add("time");
+
+	texto.textContent = mensaje.value;
+	ultMensaje.textContent = mensaje.value;
+	var fecha = new Date();
+	var hora = fecha.getHours();
+	var minutos = fecha.getMinutes();
+	var tiempo = hora + ":" + minutos;
+	contenHora.textContent = tiempo;
+	ultHora.textContent = tiempo;
+
+	cajaTexto.insertBefore(texto,cajaTexto.childNodes[0]);
+	cajaTexto.insertBefore(contenHora,cajaTexto.childNodes[1]);
+	envoltura.insertBefore(cajaTexto,envoltura.childNodes[0]);
+	contenedor.appendChild(envoltura);
+}
+function cambiaHeader(){
+	imgContacto.src = this.childNodes[1].src ;
+	nombreContacto.textContent = this.childNodes[3].textContent;
+}
 
